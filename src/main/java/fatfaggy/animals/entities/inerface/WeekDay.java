b3d2d36e4c80333d8adf17077296e13a33158d2d
andreykozhemyako <andrey_radrigo@mail.ru>
2018-10-31 20:02:24 +0400

    wer

---
+++ src/main/java/fatfaggy/animals/entities/inerface/WeekDay.java
@@ -0,0 +1,22 @@
+package fatfaggy.animals.entities.inerface;
+
+import org.springframework.context.annotation.Bean;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
+public interface WeekDay {
+    @Bean
+    public WeekDay getDay() {
+        DayOfWeek dayOfWeek = LocalDate.now().getDayOfWeek();
+        switch (dayOfWeek) {
+            case MONDAY: return new Monday();
+            case TUESDAY: return new Tuesday();
+            case WEDNESDAY: return new Wednesday();
+            case THURSDAY: return new Thursday();
+            case FRIDAY: return new Friday();
+            case SATURDAY: return new Saturday();
+            default: return new Sunday();
+        }
+    }
+}
